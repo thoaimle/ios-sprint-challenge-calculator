e6b885de890df091082040048c9e84f97dbb523a
thoaile <thoaimlevn@gmail.com>
2019-05-31 11:06:38 -0700

    rough draft

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -9,20 +9,36 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    var displayNumber:Double = 0
+    var previousNumber:Double = 0
+    var performingMath = false
+    var operation = 0
     
     @IBOutlet weak var outputLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    // MARK: - Action Handlers
-    
-    @IBAction func operandTapped(_ sender: UIButton) {
-        
-    }
+    @IBAction func numbers(_ sender: UIButton) {
+        if performingMath == true {
+            outputLabel.text = String(sender.tag-1)
+            displayNumber = Double(outputLabel.text!)!
+            performingMath = false
+        }
+        else
+        }
+            outputLabel.text = outputLabel.text! + String(sender.tag-1)
+            displayNumber = Double(outputLabel.text!)!
     
     @IBAction func operatorTapped(_ sender: UIButton) {
+        if outputLabel.text != "" && sender.tag! = 11 && sender.tag ! =16
+            previousNumber = Double(outputLabel.text!)!
+        if sender.tag == 12 // Divide {
+            outputLabel.text = "/"; {
+                else if sender.tag == 13 //Multiply {
+                outputLabel.text == "x" {
+                else if sender.tag 
+                
+            }
+            
+        }
         
     }
     
